db54453ed05838df35b39b6f300f0141f38b2bcf
benfarmercode <benfarmercode@gmail.com>
2020-12-23 19:10:17 -0600

    Fix typo in settings file

--- whats_free_on_craigslist/settings.py
+++ whats_free_on_craigslist/settings.py
@@ -23,7 +23,7 @@ TEMPLATE_DIR = BASE_DIR / 'templates'
 SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = config('DEBUG', defautl=True, cast=bool)
+DEBUG = config('DEBUG', default=True, cast=bool)
 
 ALLOWED_HOSTS = []
 
